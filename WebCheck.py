0aea0bb1eabc59eeb5186f09d03ba6430cd78123
CEILINGSPYSERVERS <CEILINGSPYSERVERS@gmail.com>
2021-09-06 00:12:26 -0500

    Update WebCheck.py

--- WebCheck.py
+++ WebCheck.py
@@ -96,7 +96,7 @@ async def log_command(ctx) -> None:
 async def IO(address, count, interval, timeout, ctx):
     try:
         try:
-            if len(ctx.message.content) > 30:
+            if len(ctx.message.content) > 100:
                 return address, False
             address = re.search("(?<=[^/]//).+", address).group(0)
         except:
